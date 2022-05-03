2e5c71933ebd4667fbf5ba428bab3bfe73d149db
Colin Mosi Cargile <cargile10@gmail.com>
2022-05-02 20:57:49 -0400

    added a big logo for the home page by creating a new component

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,13 @@
+import './index.scss';
+import LogoS from '../../../assets/images/c-letter-logo-F77FFE28B8-seeklogo.com.png';
+
+const Logo = () => {
+    
+    return ( 
+        <div className='logo-container'>
+            <img className='solid-logo' src={LogoS} alt="C"/> 
+        </div>
+    )
+} 
+
+export default Logo
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/c-letter-logo-F77FFE28B8-seeklogo.com.png';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo'; //do I need this? //
 import { useEffect, useState } from 'react'; 
 import './index.scss'; 
 
@@ -32,6 +33,7 @@ const Home = () => {
                 <h2>Front-End Developer</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
+            <Logo /> 
         </div>
     );
 }
