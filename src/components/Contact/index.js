f6621fa2ae8f2c909f7d462969c44fc3510c7100
Colin Mosi Cargile <cargile10@gmail.com>
2022-05-25 11:59:45 -0400

    completed the JS file for the contact page and now working to style the page itself with the scss file

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,14 @@
-import './index.scss';
-import Loader from 'react-loaders';
-import AnimatedLetters from '../AnimatedLetters'; 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
+    const form = useRef() 
 
     // useEffect(() => {
     //     return setTimeout(() => {
@@ -28,6 +32,27 @@ const Contact = () => {
                         I wish to continue to learn and grow from the people around me to elevate each other. Howerver, if you have other 
                         requests or questions, don't hesitate to contact me using the form below. 
                     </p>
+                    <div className="contact-form">
+                        <form>
+                            <ul>
+                                <li className="half">
+                                    <input type="text" name="name" placeholder="Name" required />
+                                </li>
+                                <li className="half">
+                                    <input type="email" name="email" placeholder="Email" required />
+                                </li>
+                                <li>
+                                    <input placehoder="Subject" type="text" name="subject" required /> 
+                                </li>
+                                <li>
+                                    <textarea placeholder="Message" name="message" required></textarea>
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
                 </div>
             </div>
             <Loader type="pacman" />
